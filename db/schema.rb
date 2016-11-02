3d9e959c03cce4bea148c201e2cfa089d6440c96
nazgus <gus@deleon.org.mx>
2016-11-01 22:58:30 -0700

    Create Pokemon model and trainer association

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141018092449) do
+ActiveRecord::Schema.define(version: 20161102055143) do
+
+  create_table "pokemons", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "level"
+    t.integer  "trainer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "trainers", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
